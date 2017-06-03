aa70ffbecad6b68f54cee90b9c52bd86cc8bcfb8
Kleinmolsen <15689666217.@163.com>
2017-06-03 15:03:08 +0800

    first commit

---
+++ doudizhu/src/components/GameStart.vue
@@ -0,0 +1,37 @@
+<template>
+	<div>
+		<el-row style="margin-bottom: 18px">
+			<el-col :span='24' style='text-align: center'>欢迎来斗地主</el-col>
+		</el-row>
+		<el-row>
+			<el-col :span="4">&nbsp;</el-col>
+			<el-col :span="16" style='align-content: center;border:1px solid #99a9bf;padding: 21px;'>
+				<el-form ref="form">
+					<el-form-item style='text-align: center;margin:0px;'>
+						登陆
+					</el-form-item>
+				  <el-form-item >
+				    <el-input placeholder="请输入email" style='margin-bottom:15px'></el-input>
+				    <el-input placeholder="请输入密码" type='password'></el-input>
+				  </el-form-item>
+				  <el-form-item style='text-align: center'>
+				    <el-button type="primary">登陆</el-button>
+				    <router-link to="/register">
+				    	<el-button>注册</el-button>
+				    </router-link>
+				  </el-form-item>
+				</el-form>
+			</el-col>
+			<el-col :span="4">&nbsp;</el-col>
+		</el-row>
+	</div>
+</template>
+<script>
+	export default {
+	  data () {
+	    return {
+	      
+	    }
+	  }
+	}
+</script>
